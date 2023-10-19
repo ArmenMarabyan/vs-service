f5ffea72485a66fd726036e8a6968c157fe54e58
Armen <marabyan98@mail.ru>
2023-10-19 23:40:35 +0400

    Added bootstrap

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function index(Request $request): Response
+    {
+
+        $term = $request->get('q');
+
+        dd($term);
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+}
